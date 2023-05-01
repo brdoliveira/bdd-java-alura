7c63220cd622c2773ee04daf2f689587cde27bb1
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-05-01 20:35:37 -0300

    FEAT(LeilaoSteps): Implementando testes do leilão

---
+++ src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
@@ -0,0 +1,42 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import br.com.alura.leilao.e2e.pages.Browser;
+import br.com.alura.leilao.e2e.pages.LeiloesPage;
+import br.com.alura.leilao.e2e.pages.LoginPage;
+import br.com.alura.leilao.e2e.pages.NovoLeilaoPage;
+import org.junit.Assert;
+
+
+public class LeilaoSteps implements io.cucumber.java8.Pt{
+
+    private LoginPage loginPage;
+    private LeiloesPage leiloesPage;
+    private NovoLeilaoPage novoLeilaoPage;
+    private Browser browser;
+
+    public LeilaoSteps() {
+        Dado("um usuario logado", () -> {
+            this.browser = new Browser();
+            browser.seed();
+            loginPage = browser.getLoginPage();
+            leiloesPage = loginPage.realizaLoginComoFulano();
+        });
+
+        Quando("acessa a pagina de novo leilao", () -> {
+            novoLeilaoPage = this.leiloesPage.visitaPaginaParaCriarUmNovoLeilao();
+        });
+
+        Quando("prenche o formulario com dados validos", () -> {
+            this.leiloesPage = this.novoLeilaoPage.preencheForm("PC Novo", "1500", "01/11/2020");
+        });
+
+        Entao("volta para a pagina de leiloes", () -> {
+            Assert.assertTrue(this.leiloesPage.estaNaPaginaDeLeiloes());
+        });
+
+        Entao("o novo leilao aparece na tabela", () -> {
+            this.leiloesPage.existe("PC Novo","01/11/2020","1500", "fulano");
+            this.browser.clean();
+        });
+    }
+}
