c60000502ccf95086b9f6e59664e12e8557afaa3
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-30 22:29:45 -0300

    FEAT(PropondoLanceSteps): Criando novos testes

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.acceptance.steps;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
@@ -11,6 +12,8 @@ import org.junit.Assert;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PropondoLanceSteps {
     private Lance lance;
@@ -84,5 +87,43 @@ public class PropondoLanceSteps {
                 this.listaLance.get(1).getValor(),
                 leilao.getLances().get(1).getValor()
         );
+    }
+
+    @Dado("um lance de {double} reais e do usuario {string}")
+    public void um_lance_de_reais(Double valor, String nomeUsuario) {
+        this.lance = new Lance(
+                new Usuario(nomeUsuario),
+                new BigDecimal(valor)
+        );
+    }
+
+    @Entao("o lance nao eh aceito")
+    public void o_lance_nao_eh_aceito() {
+        Assert.assertEquals(
+        0,
+            leilao.getLances().size()
+        );
+    }
+
+    @Dado("dois lances")
+    public void dois_lances(DataTable dataTable) {
+        List<Map<String,String>> listaInformacoes = dataTable.asMaps();
+        for (Map<String, String> mapa : listaInformacoes){
+            String nomeUsuario = mapa.get("nomeUsuario");
+            Double valor = Double.valueOf(mapa.get("valor"));
+
+            this.listaLance.add(
+                    new Lance(
+                            new Usuario(nomeUsuario),
+                            new BigDecimal(valor)
+                    )
+            );
+        }
+    }
+
+    @Entao("o segundo lance nao eh aceitos")
+    public void o_segundo_lance_nao_eh_aceitos() {
+        Assert.assertEquals(1, leilao.getLances().size());
+        Assert.assertEquals(this.listaLance.get(0).getValor(),leilao.getLances().get(0).getValor());
     }
 }
